54bd072eb6f6512f9182058a8b6edf06176778c5
AngelOntM <ajom0507@hotmail.com>
2024-04-07 16:47:33 -0600

    Mensaje

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,6 +30,9 @@
                 <span class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 ">
                     {{Auth::user()->rol->role}}
                 </span>
+                <span class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 ">
+                    {{DB::getDefaultConnection();}}
+                </span>
             </div>
 
             <!-- Settings Dropdown -->
